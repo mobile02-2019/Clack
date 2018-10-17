700f5abb902e2e6d1d75d95a639d8dee714e60e2
Lucas Prado <lptd99@gmail.com>
2018-10-17 16:47:47 -0300

    Login V3

--- ClackApp/app/src/main/java/br/com/digitalhouse/clackapp/LoginActivity.java
+++ ClackApp/app/src/main/java/br/com/digitalhouse/clackapp/LoginActivity.java
@@ -1,7 +1,9 @@
 package br.com.digitalhouse.clackapp;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class LoginActivity extends Activity {
 
@@ -9,5 +11,11 @@ public class LoginActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+
+    public void irParaPreferencesGenerico(View view) {
+        Intent intent = new Intent(this, PreferenceActivity.class);
+        startActivity(intent);
     }
 }
