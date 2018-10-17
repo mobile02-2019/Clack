a230f090fa71db66268d10bd00515782fe9856e6
rodrigoctavares <ctavaresrodrigo@gmail.com>
2018-10-17 15:15:00 -0300

    home

---
+++ ClackApp/app/src/main/java/br/com/digitalhouse/clackapp/HomeActivity.java
@@ -0,0 +1,21 @@
+package br.com.digitalhouse.clackapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+
+    public void filme_escolhido(View view) {
+
+        Intent intent = new Intent(this,DetailActivity.class);
+
+    }
+}
