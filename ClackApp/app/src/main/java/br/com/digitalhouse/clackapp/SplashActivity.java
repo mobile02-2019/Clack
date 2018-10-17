bc822fa13ce94ac54b07569d775cb7fe9dedf447
ademirpaj <ademir.paj@hotmail.com>
2018-10-17 15:56:37 -0300

    manifest

--- ClackApp/app/src/main/java/br/com/digitalhouse/clackapp/SplashActivity.java
+++ ClackApp/app/src/main/java/br/com/digitalhouse/clackapp/SplashActivity.java
@@ -1,6 +1,31 @@
 package br.com.digitalhouse.clackapp;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class SplashActivity extends Activity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Timer().schedule(new TimerTask() {
+            @Override
+            public void run() {
+                irParaLogin();
+            }
+        }, 3000);
+
+    }
+
+    public void irParaLogin(){
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+    }
 }
