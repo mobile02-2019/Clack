f5614d370d0b59a0041f636ff6bcd3c8e0fc4a4e
rodrigoctavares <ctavaresrodrigo@gmail.com>
2018-10-30 16:36:06 -0300

    home fragment

--- ClackApp/app/src/main/java/br/com/digitalhouse/clackapp/MainActivity.java
+++ ClackApp/app/src/main/java/br/com/digitalhouse/clackapp/MainActivity.java
@@ -8,33 +8,121 @@ import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import br.com.digitalhouse.clackapp.adapter.FragmentViewPagerAdapter;
 import br.com.digitalhouse.clackapp.fragment.FavoritosFragment;
 import br.com.digitalhouse.clackapp.fragments.ConfiguracoesFragment;
+import br.com.digitalhouse.clackapp.fragments.HomeFragment;
 import br.com.digitalhouse.clackapp.fragments.PesquisaFragment;
 
 public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
 
     private BottomNavigationView navigationView;
+    MenuItem prevMenuItem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        final ViewPager viewPager = findViewById(R.id.viewpager_id);
+
+        List<Fragment> fragmentList = new ArrayList<>();
+        fragmentList.add(new HomeFragment());
+        fragmentList.add(new FavoritosFragment());
+        fragmentList.add(new PesquisaFragment());
+        fragmentList.add(new ConfiguracoesFragment());
+
+        FragmentViewPagerAdapter adapter = new FragmentViewPagerAdapter(getSupportFragmentManager(),fragmentList);
+
+        viewPager.setAdapter(adapter);
+        viewPager.setCurrentItem(0);
+
         navigationView = (BottomNavigationView) findViewById(R.id.navigationView);
-        navigationView.setOnNavigationItemSelectedListener(this);
+
+        navigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+                switch (menuItem.getItemId()){
+                    case R.id.navigation_home: {
+                        viewPager.setCurrentItem(0);
+                        break;
+                    }
+                    case R.id.navigation_favoritos: {
+                        viewPager.setCurrentItem(1);
+                        break;
+                    }
+                    case R.id.navigation_pesquisa: {
+                        viewPager.setCurrentItem(2);
+                        break;
+                    }
+                    case R.id.navigation_config: {
+                        viewPager.setCurrentItem(3);
+                        break;
+                    }
+
+                }
+
+                return true;
+            }
+        });
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                if (prevMenuItem != null) {
+                    prevMenuItem.setChecked(false);
+                } else {
+                    navigationView.getMenu().getItem(0).setChecked(false);
+                }
+                Log.d("page", "onPageSelected: " + position);
+                navigationView.getMenu().getItem(position).setChecked(true);
+                prevMenuItem = navigationView.getMenu().getItem(position);
+
+            }
+
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+
+
 
     }
+
+
+
+    public void replaceFragment (Fragment fragment){
+       FragmentManager manager = getSupportFragmentManager();
+       FragmentTransaction transaction = manager.beginTransaction();
+       transaction.replace(R.id.container_main_id,fragment);
+       transaction.commit();
+    }
+
+
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
 
         switch (menuItem.getItemId()){
             case R.id.navigation_home: {
-                Toast.makeText(this, "Você já está nessa tela.", Toast.LENGTH_SHORT).show();
+                replaceFragment(new HomeFragment());
                 break;
             }
             case R.id.navigation_favoritos: {
@@ -54,14 +142,6 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
 
         return true;
     }
-
-    public void replaceFragment (Fragment fragment){
-       FragmentManager manager = getSupportFragmentManager();
-       FragmentTransaction transaction = manager.beginTransaction();
-       transaction.replace(R.id.container_main_id,fragment);
-       transaction.commit();
-    }
-
 
 
 }
--- ClackApp/app/src/main/java/br/com/digitalhouse/clackapp/fragments/HomeFragment.java
+++ ClackApp/app/src/main/java/br/com/digitalhouse/clackapp/fragments/HomeFragment.java
@@ -1,21 +1,26 @@
-package br.com.digitalhouse.clackapp;
+package br.com.digitalhouse.clackapp.fragments;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import br.com.digitalhouse.clackapp.DetailActivity;
 import br.com.digitalhouse.clackapp.Elementos.Movie;
+import br.com.digitalhouse.clackapp.R;
 import br.com.digitalhouse.clackapp.adapter.RecyclerViewMovieAdapter;
 import br.com.digitalhouse.clackapp.adapter.RecyclerViewMovieAdapter2;
 import br.com.digitalhouse.clackapp.adapter.RecyclerViewMovieAdapter3;
 import br.com.digitalhouse.clackapp.adapter.RecyclerViewMovieAdapter4;
 
-public class HomeActivity extends AppCompatActivity implements RecyclerViewMovieAdapter.CardMovieClicado, RecyclerViewMovieAdapter2.CardMovieClicado, RecyclerViewMovieAdapter3.CardMovieClicado, RecyclerViewMovieAdapter4.CardMovieClicado {
+public class HomeFragment extends Fragment implements RecyclerViewMovieAdapter.CardMovieClicado, RecyclerViewMovieAdapter2.CardMovieClicado, RecyclerViewMovieAdapter3.CardMovieClicado, RecyclerViewMovieAdapter4.CardMovieClicado {
 
     public static final String MOVIE_TITULO = "movie_titulo";
     private RecyclerView recyclerView,recyclerView2,recyclerView3,recyclerView4;
@@ -23,30 +28,31 @@ public class HomeActivity extends AppCompatActivity implements RecyclerViewMovie
 
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
 
-        recyclerView = findViewById(R.id.recycler_view_id);
-        recyclerView2 = findViewById(R.id.recycler_view_id_2);
-        recyclerView3 = findViewById(R.id.recycler_view_id_3);
-        recyclerView4 = findViewById(R.id.recycler_view_id_4);
+        recyclerView = view.findViewById(R.id.recycler_view_id);
+        recyclerView2 = view.findViewById(R.id.recycler_view_id_2);
+        recyclerView3 = view.findViewById(R.id.recycler_view_id_3);
+        recyclerView4 = view.findViewById(R.id.recycler_view_id_4);
 
         RecyclerViewMovieAdapter adapter = new RecyclerViewMovieAdapter(getListMovie());
 
-        LinearLayoutManager manager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL,false);
+        LinearLayoutManager manager = new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL,false);
 
         RecyclerViewMovieAdapter2 adapter2 = new RecyclerViewMovieAdapter2(getListMovie());
 
-        LinearLayoutManager manager2 = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL,false);
+        LinearLayoutManager manager2 = new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL,false);
 
         RecyclerViewMovieAdapter3 adapter3 = new RecyclerViewMovieAdapter3(getListMovie());
 
-        LinearLayoutManager manager3 = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL,false);
+        LinearLayoutManager manager3 = new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL,false);
 
         RecyclerViewMovieAdapter4 adapter4 = new RecyclerViewMovieAdapter4(getListMovie());
 
-        LinearLayoutManager manager4 = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL,false);
+        LinearLayoutManager manager4 = new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL,false);
 
         recyclerView.setLayoutManager(manager);
         recyclerView.setAdapter(adapter);
@@ -61,7 +67,7 @@ public class HomeActivity extends AppCompatActivity implements RecyclerViewMovie
         recyclerView4.setAdapter(adapter4);
 
 
-
+        return view;
     }
 
 
@@ -97,7 +103,7 @@ public class HomeActivity extends AppCompatActivity implements RecyclerViewMovie
 
         bundle.putString(MOVIE_TITULO,movie.getNome());
 
-        Intent intent = new Intent(this,DetailActivity.class);
+        Intent intent = new Intent(getContext(),DetailActivity.class);
         intent.putExtras(bundle);
         startActivity(intent);
 
